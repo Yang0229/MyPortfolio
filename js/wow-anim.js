2df93d520cf05af2f8e949bbd205b9bf72dba423
tinker <yang0229@algonquinlive.com>
2018-10-28 17:24:44 -0400

    MyPortfolio 1.0
    2018-10-28

---
+++ js/wow-anim.js
@@ -0,0 +1,17 @@
+/****************************************************************
+ *																*		
+ * 						      Bo Yang							*
+ *                           Protfolio						    *
+ * 																*
+****************************************************************/
+
+wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       50,          // default
+                      mobile:       true,       // default
+                      live:         true     // default
+                    }
+                    );
+                    wow.init();
